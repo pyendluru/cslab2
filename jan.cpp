f7b3e1b49bf6377244c1a774ddb60ae12e9e029f
Pavithra Yendluru <yendluru@usc.edu>
2018-01-20 12:03:45 -0800

    hellooo

--- jan.cpp
+++ jan.cpp
@@ -4,22 +4,23 @@ using namespace std;
 
 //This function will print file with words reversed.
 
-void PrintReverse(ifstream &stream, int length)
+void PrintReverse(ifstream &stream, int Stringcount)
 {
     string letters;
     stream >> letters;
-        PrintReverse(stream, length-1);
+    if(Stringcount > 0)
+        PrintReverse(stream, Stringcount-1);
     cout << letters << " ";  
 }
+
 /* The main below will first open text file from the command line.
 Then it will extract first value as number of strings.
 Then finally uses the function above to print in reverse*/
 
-int main(int word, char* words[])
+int main(int word, int count, char* words[])
 {
     ifstream stream;
-    stream.open(words[1]);   
-    int count;      
+    stream.open(words[1]);         
     stream >> count;               
     PrintReverse(stream, count); 
     cout << endl;
