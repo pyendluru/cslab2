c50ad9b82e84f2ed42692203083e8ad27f0103d8
Pavithra Yendluru <yendluru@usc.edu>
2018-01-19 19:17:53 -0800

    git i love it

--- fun.cpp
+++ fun.cpp
@@ -3,12 +3,12 @@ void funcA(int*,int);
 void funcB(int *a,int min,int max);
 using namespace std;
 
-int main(
+int main()
 {
 	int a[5]*{1,2,3,4,5};
 	funcA(a,5);
 	return 0;
-})
+}
 
 void funcA (int *a, int size) {
     funcB(a, 0, size-1);
