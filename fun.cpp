07b0d91a4c4c75737af532216068c4473f89f25a
Pavithra Yendluru <yendluru@usc.edu>
2018-01-19 19:16:07 -0800

    love it

--- fun.cpp
+++ fun.cpp
@@ -1,4 +1,14 @@
 #include <iostream>
+void funcA(int*,int);
+void funcB(int *a,int min,int max);
+using namespace std;
+
+int main(
+{
+	int a[5]*{1,2,3,4,5};
+	funcA(a,5);
+	return 0;
+})
 
 void funcA (int *a, int size) {
     funcB(a, 0, size-1);
