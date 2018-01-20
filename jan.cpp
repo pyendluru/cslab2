1e21a589eecde3fe837b81d9f3cd66fc3a0f9c3c
Pavithra Yendluru <yendluru@usc.edu>
2018-01-20 11:35:02 -0800

    hello

--- jan.cpp
+++ jan.cpp
@@ -3,22 +3,22 @@
 using namespace std;
 
 //This function will print file with words reversed.
-void PrintReverse(ifstream &fin, int length)
+void PrintReverse(ifstream &stream, int length)
 {
     string letters;
-    fin >> letters;
+    stream >> letters;
     if(length > 0)
-        PrintReverse(fin, length-1);
+        PrintReverse(stream, length-1);
     cout << letters << " ";  
 }
 
 int main(int word, char* words[])
 {
-    ifstream fin;
-    fin.open(words[1]);   //From, the terminal will open the hw1q6.txt file
+    ifstream stream;
+    stream.open(words[1]);   //From, the terminal will open the hw1q6.txt file
     int count;      
-    fin >> count;               //Will extract first value from file
-    PrintReverse(fin, count);   //The recursive function will print 
+    stream >> count;               //Will extract first value from file
+    PrintReverse(stream, count);   //The recursive function will print 
     cout << endl;
     return 0;
 }
